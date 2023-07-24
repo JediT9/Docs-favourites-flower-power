8a5e033fa93504f195bd08d5586c8ca50d993d8a
JediT9 <82427185+JediT9@users.noreply.github.com>
2023-07-25 10:24:34 +1200

    Started building code for 1st trial method of calculating the object's path
    Created initial variables and constants
    Next step is to calculate initial velocities from the angle and the initial net velocity.

--- test.gd
+++ test.gd
@@ -1,7 +1,26 @@
 extends Sprite2D
 
 func _init():
-	print("Hello world")
+	pass
+	
+# Trial option 1 for calculating the path recalculating every time step
+func trial_1(initial_velocity, initial_angle):
+	
+	# Set up variables
+	var x_position: float = 0.0
+	var y_position: float = 0.0
+	var x_velocity: float
+	var y_velocity: float
+	var time: int = 0
+	var time_step: float = 0.02
+	
+	# Define constants
+	const Y_ACCELERATION: float = 9.81
+	const X_ACCELERATION: float = 0.0
+	
+	# Calculate initial velocities
+	
+	
 
 
 # Called when the node enters the scene tree for the first time.
