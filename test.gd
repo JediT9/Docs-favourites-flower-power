704a602f8adde42419c6f98bcdcec5cdad70d3db
JediT9 <82427185+JediT9@users.noreply.github.com>
2023-07-27 12:13:04 +1200

    Almost finished trial one for path
    Integrated path calculations into the built in _process, need to retest with acceleration negative to test.

--- test.gd
+++ test.gd
@@ -1,27 +1,18 @@
 extends Sprite2D
+const Y_ACCELERATION: float = -9.81
+const X_ACCELERATION: float = 0.0
+var time: float = 0
+var x_position: float = 0.0
+var y_position: float = 0.0
+var initial_velocity: float = 10
+var initial_angle: float = deg_to_rad(45)
+var x_velocity: float = initial_velocity * cos(initial_angle)
+var y_velocity: float = initial_velocity * sin(initial_angle)
 
 func _init():
+	
 	pass
 	
-# Trial option 1 for calculating the path recalculating every time step
-func trial_1(initial_velocity, initial_angle):
-	
-	# Set up variables
-	var x_position: float = 0.0
-	var y_position: float = 0.0
-	var x_velocity: float
-	var y_velocity: float
-	var time: int = 0
-	var time_step: float = 0.02
-	
-	# Define constants
-	const Y_ACCELERATION: float = 9.81
-	const X_ACCELERATION: float = 0.0
-	
-	# Calculate initial velocities
-	
-	
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,4 +21,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# Update velocities and positions
+	y_velocity += (Y_ACCELERATION * delta)
+	x_velocity += (X_ACCELERATION * delta)
+	x_position += x_velocity * delta
+	y_position += y_velocity * delta
+	
+	print(x_velocity)
+	print(y_velocity)
+	print(x_position)
+	print(y_position)
+	print()
+	position.x = x_position
+	position.y = y_position
