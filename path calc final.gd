4658ae44c3c7c81ea89707e9a8fcd2aef87f0240
JediT9 <82427185+JediT9@users.noreply.github.com>
2023-08-06 14:33:54 +1200

    Thoroughly tested the final path calc
    Carried out full boundary and expected inputs for the initial velocity and angle, however did not test invalid as the valid input checker has not yet been built, so invalid inputs will be filtered out before reaching this piece of code.

--- path calc final.gd
+++ path calc final.gd
@@ -4,8 +4,8 @@ const Y_ACCELERATION: float = -9.81
 const X_ACCELERATION: float = 0.0
 const ACCELERATION_TO_EQUATION: int = 2
 var time: float = 0
-var initial_velocity: float = 10
-var initial_angle: float = deg_to_rad(45)
+var initial_velocity: float = 100
+var initial_angle: float = deg_to_rad(1)
 var y_distance: float
 var x_velocity_initial: float = initial_velocity * cos(initial_angle)
 var y_velocity_initial: float = initial_velocity * sin(initial_angle)
@@ -18,3 +18,5 @@ func _process(delta):
 	y_distance = time*y_velocity_initial + ACCELERATION_MULTIPLIER*Y_ACCELERATION*pow(time, ACCELERATION_TO_EQUATION)
 	position.y = y_distance
 	position.x = x_velocity_initial * time
+	if y_distance <= 0:
+		print(time) 
