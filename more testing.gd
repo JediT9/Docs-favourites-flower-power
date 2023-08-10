56bcc58e1f6eece22a5558d0e68cf41be490a1cc
JediT9 <82427185+JediT9@users.noreply.github.com>
2023-08-10 12:13:46 +1200

    Started trying to redo sprite structure

---
+++ more testing.gd
@@ -0,0 +1,11 @@
+extends CollisionPolygon2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position.x = 30
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x -= 2
