9aa30514e8d2804ebb9b92137736769348a0605d
matthemi2 <milly.matthews@student.onslow.school.nz>
2023-08-18 12:55:54 +1200

    More Character Learning
    We managed to get the character moving now, which is a good start.

--- milly_learning/player_test.gd
+++ milly_learning/player_test.gd
@@ -1,21 +1,22 @@
-extends CharacterBody2D
+extends Sprite2D
 
 var score : int = 0
-var speed : int = 200
+var speed : int = 2
 var jumpForce : int = 600
 var gravity : int = 800
 var vel : Vector2 = Vector2()
-var sprite : Sprite = $Sprite
+@onready
+var sprite : Sprite2D = $Sprite
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	vel.x = 0
   #movement inputs
 	if Input.is_action_pressed("move_left"):
-		vel.x -= speed
+		position.x -= speed
 	if Input.is_action_pressed("move_right"):
-		vel.x += speed
+		position.x += speed
