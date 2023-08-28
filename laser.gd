403e1ce4275ec3480672adfcc2cc46137081032f
JediT9 <82427185+JediT9@users.noreply.github.com>
2023-08-29 10:24:40 +1200

    Experimented with gd classes
    Learnt how gd uses classes, and how to initialize scripts as an object.

--- laser.gd
+++ laser.gd
@@ -1,16 +1,23 @@
-extends Sprite2D
+class_name laser extends Sprite2D
 
+# Initiate variable
+var centralPosition: Array
+var moving: bool
+var size: Array
+var movementRange: int = 0
+var timeElapsed: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position.x = 300
+	position.x = centralPosition[0]
+	position.y = centralPosition[1]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x -= 20 * float(delta)
+	timeElapsed += delta
+	position.y += movementRange*sin(timeElapsed)*delta
 
 
-func _on_area_2d_2_area_entered(area):
-	print("collided")
-	self.hide()
+func _on_laser_area_area_entered(area):
+	pass # Will end the game
