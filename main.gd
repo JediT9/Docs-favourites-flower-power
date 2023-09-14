4867b3b7aedb50aa9823d65e5bb4f92e43aea0b5
JediT9 <82427185+JediT9@users.noreply.github.com>
2023-09-14 16:22:53 +1200

    Built model for spikes
    Created an equivalent of the laser class for spikes, currently uses rectangle shape

--- main.gd
+++ main.gd
@@ -1,17 +1,19 @@
 extends Node2D
 
-var laser_class
+var laser_class: laser_object
 var path_class: projectile_path_calc
+var spike_class: spike_object
 var position_stuff
 
 func _ready():
 	# Load classes
 	# laser_class = load("res://laser.gd").new([100, 100], [100, 100], load("res://sprites/idle.png"), 100, self)
 	path_class = load("res://path calc final.gd").new(100, 1, 0, 0)
+	spike_class = load("res://spike.gd").new([600, 600], [50, 50], self, load("res://sprites/jump.png"))
 	
 func _process(delta):
+	# gets the position values for the character
 	position_stuff = path_class.calc_positions(delta)
-	print(position_stuff)
 	
 func get_values():
 	return [laser_class.central_position, laser_class.movement_range]
