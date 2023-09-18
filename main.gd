51e7f42320b007a1fb6be3496f242250aac62291
JediT9 <82427185+JediT9@users.noreply.github.com>
2023-09-18 13:02:17 +1200

    Run reformatter on project to make sure all files are GD style correct

--- main.gd
+++ main.gd
@@ -5,15 +5,22 @@ var path_class: projectile_path_calc
 var spike_class: spike_object
 var position_stuff
 
+
 func _ready():
 	# Load classes
-	# laser_class = load("res://laser.gd").new([100, 100], [100, 100], load("res://sprites/idle.png"), 100, self)
+	laser_class = load("res://laser.gd").new(
+		[100, 100], [100, 100], load("res://sprites/idle.png"), 100, self
+	)
 	path_class = load("res://path calc final.gd").new(100, 1, 0, 0)
-	spike_class = load("res://spike.gd").new([600, 600], [50, 50], self, load("res://sprites/jump.png"))
-	
+	spike_class = load("res://spike.gd").new(
+		[600, 600], [50, 50], self, load("res://sprites/jump.png")
+	)
+
+
 func _process(delta):
 	# gets the position values for the character
 	position_stuff = path_class.calc_positions(delta)
-	
+
+
 func get_values():
 	return [laser_class.central_position, laser_class.movement_range]
--- spike.gd
+++ spike.gd
@@ -5,43 +5,43 @@ var center_position: Array
 var size: Array
 var head_node: Node2D
 
+
 func _init(central_position, spike_size, node, texture):
-	
 	# Check variables contain correct datatypes
 	if typeof(central_position) != TYPE_ARRAY:
 		# Call reset function
 		pass
-		
+
 	if typeof(spike_size) != TYPE_ARRAY:
 		# Call reset function
 		pass
-		
+
 	if typeof(node) != TYPE_OBJECT:
 		# Call reset function
 		pass
-		
+
 	if typeof(texture) != TYPE_OBJECT:
 		# Call reset function
 		pass
-		
+
 	# Assign values to class data
 	center_position = central_position
 	size = spike_size
 	head_node = node
-	
+
 	# Create nodes
 	var spike_sprite: Sprite2D = Sprite2D.new()
 	var spike_area: Area2D = Area2D.new()
 	var spike_collision_box: CollisionShape2D = CollisionShape2D.new()
 	var spike_shape: RectangleShape2D = RectangleShape2D.new()
-	
+
 	# Assign values to nodes
 	spike_sprite.texture = texture
 	spike_shape.size = Vector2(size[0], size[1])
 	spike_collision_box.shape = spike_shape
 	spike_sprite.position.x = central_position[0]
 	spike_sprite.position.y = central_position[1]
-	
+
 	# Add nodes to scene tree
 	spike_area.add_child(spike_collision_box)
 	spike_sprite.add_child(spike_area)
