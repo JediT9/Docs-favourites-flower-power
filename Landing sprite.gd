7b0ccb3cdfa63f57680d2de29b1d235d317853ff
JediT9 <82427185+JediT9@users.noreply.github.com>
2023-10-17 12:20:02 +1300

    Landing page works

--- Landing sprite.gd
+++ Landing sprite.gd
@@ -11,9 +11,34 @@ func _ready():
 	position.x = DisplayServer.window_get_size()[0] / 2.0
 	scale.x = (DisplayServer.window_get_size()[0]) / image_width
 	scale.y = (DisplayServer.window_get_size()[1]) / image_height
-	
+
 
 
 func _on_tutorial_button_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("left_click"):
-		pass   # Change level
+		get_tree().change_scene_to_file("res://Tutorial level.tscn")
+
+
+func _on_level_1_button_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("left_click"):
+		get_tree().change_scene_to_file("res://Level 1.tscn")
+
+
+func _on_level_2_button_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("left_click"):
+		get_tree().change_scene_to_file("res://Level 2.tscn")
+
+
+func _on_level_3_button_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("left_click"):
+		get_tree().change_scene_to_file("res://Level 3.tscn")
+
+
+func _on_level_4_button_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("left_click"):
+		get_tree().change_scene_to_file("res://Level 4.tscn")
+
+
+func _on_level_5_button_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("left_click"):
+		get_tree().change_scene_to_file("res://Level 5.tscn")
