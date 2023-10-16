b9e609248af3588009f9489e3e475d70e1bf055e
JediT9 <82427185+JediT9@users.noreply.github.com>
2023-10-17 10:24:36 +1300

    Started setting up collision areas for the landing page

---
+++ Landing sprite.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+const image_height: float = 794
+const image_width: float = 1123
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	# center on the screen
+	position.y = DisplayServer.window_get_size()[1] / 2.0
+	position.x = DisplayServer.window_get_size()[0] / 2.0
+	scale.x = (DisplayServer.window_get_size()[0]) / image_width
+	scale.y = (DisplayServer.window_get_size()[1]) / image_height
+	
