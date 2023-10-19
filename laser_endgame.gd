f34a1ef0c0fd3eb5939a3a5b085dc176b84916a8
JediT9 <82427185+JediT9@users.noreply.github.com>
2023-10-20 11:29:57 +1300

    Started laser timers

--- laser_endgame.gd
+++ laser_endgame.gd
@@ -2,8 +2,16 @@ extends Node2D
 
 # Export the laser's movement value
 @export var laser_movement: int = 0
+@export var is_timer: bool = false
+@export var level_time: int = 0
 var time: float = 0
 
+
+# Called when the laser enters the scene tree
+func _ready():
+	# Check if the laser is the timer
+	if is_timer == true:
+		pass
 
 # Called when the character enters the laser
 func _on_laser_area_area_entered(area: Node2D) -> void:
@@ -16,7 +24,7 @@ func _on_laser_area_area_entered(area: Node2D) -> void:
 
 # Make the laser move
 func _physics_process(delta) -> void:
-	if laser_movement != 0:
+	if laser_movement != 0 and is_timer == false:
 		time += delta
 		# Set the new y-position
 		position.y += laser_movement * cos(time) * delta
