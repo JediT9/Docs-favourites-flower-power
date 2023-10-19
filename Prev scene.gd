67d34493a67ddfdac1138e95275c32dc423b08c2
JediT9 <82427185+JediT9@users.noreply.github.com>
2023-10-19 15:08:49 +1300

    Programmed the victory and death screens

---
+++ Prev scene.gd
@@ -0,0 +1,4 @@
+extends Node
+
+# Define the current scene
+var previous_scene: = "res://Tutorial level.tscn"
--- laser_endgame.gd
+++ laser_endgame.gd
@@ -7,11 +7,16 @@ var time: float = 0
 
 # Called when the character enters the laser
 func _on_laser_area_area_entered(area: Node2D) -> void:
-	get_tree().quit()
-
+	
+	# Set the previous scene to the current level
+	PrevScene.previous_scene = get_tree().current_scene.scene_file_path
+	
+	# Change to the death screen
+	get_tree().change_scene_to_file("res://Death page.tscn")
 
 # Make the laser move
 func _physics_process(delta) -> void:
 	if laser_movement != 0:
 		time += delta
+		# Set the new y-position
 		position.y += laser_movement * cos(time) * delta
